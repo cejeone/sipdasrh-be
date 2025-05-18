c632690b1cec46c704e4b17ba677cfff6391d9a7
ihsan <ihsan.nt@gmail.com>
2025-05-18 16:59:21 +0700

    Update DokumenFileRepository to use UUID as the identifier type

--- rh-service/src/main/java/com/kehutanan/rh/dokumen/repository/DokumenFileRepository.java
+++ rh-service/src/main/java/com/kehutanan/rh/dokumen/repository/DokumenFileRepository.java
@@ -1,7 +1,10 @@
 package com.kehutanan.rh.dokumen.repository;
 
 import com.kehutanan.rh.dokumen.model.DokumenFile;
+
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DokumenFileRepository extends JpaRepository<DokumenFile, Long> {
+public interface DokumenFileRepository extends JpaRepository<DokumenFile, UUID> {
 }
