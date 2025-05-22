7cc988152e5317491cfca8163f4ac363f96b4ba3
ihsan <ihsan.nt@gmail.com>
2025-05-22 10:21:24 +0700

    feat: Integrate Redis caching support with configuration and cache management service

--- rh-service/src/main/java/com/kehutanan/rh/RhApplication.java
+++ rh-service/src/main/java/com/kehutanan/rh/RhApplication.java
@@ -2,10 +2,12 @@ package com.kehutanan.rh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@EnableCaching
 @OpenAPIDefinition(
     info = @Info(
         title = "RH Service API",
