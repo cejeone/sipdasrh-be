63f427da5e7e0402b85a052d3dc95fa68e749922
ihsan <ihsan.nt@gmail.com>
2025-06-05 14:04:42 +0700

    refactor: Perbarui struktur DTO untuk berbagai halaman agar dapat menggunakan item yang disematkan dan ditautkan

--- pepdas-service/src/main/java/com/kehutanan/pepdas/pemantauandas/dto/PemantauanDasPageDTO.java
+++ pepdas-service/src/main/java/com/kehutanan/pepdas/pemantauandas/dto/PemantauanDasPageDTO.java
@@ -2,13 +2,16 @@ package com.kehutanan.pepdas.pemantauandas.dto;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.data.domain.Page;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.PagedModel;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.kehutanan.pepdas.pemantauandas.model.PemantauanDas;
 import com.kehutanan.pepdas.pemantauandas.model.dto.PemantauanDasDTO;
 
@@ -18,50 +21,74 @@ import lombok.Data;
 public class PemantauanDasPageDTO implements Serializable {
     private static final long serialVersionUID = 1L;
     
-    private List<PemantauanDasDTO> content;
-    private PageMetadata metadata;
-    private Links links;
+    @JsonProperty("_embedded")
+    private Embedded embedded;
+    
+    @JsonProperty("_links")
+    private Map<String, LinkItem> links;
+    
+    @JsonProperty("page")
+    private PageMetadata page;
     
     /**
      * Default constructor required for JSON deserialization
      */
     public PemantauanDasPageDTO() {
-        this.content = new ArrayList<>();
-        this.metadata = new PageMetadata(0, 0, 0, 0);
-        this.links = new Links();
+        this.embedded = new Embedded(new ArrayList<>());
+        this.links = new HashMap<>();
+        this.page = new PageMetadata(0, 0, 0, 0);
     }
     
     public PemantauanDasPageDTO(Page<PemantauanDas> page, PagedModel.PageMetadata pageMetadata, List<Link> links) {
-        this.content = page.getContent().stream()
+        // Set embedded content
+        List<PemantauanDasDTO> pemantauanDasList = page.getContent().stream()
                 .map(PemantauanDasDTO::new)
                 .collect(Collectors.toList());
+        this.embedded = new Embedded(pemantauanDasList);
         
-        this.metadata = new PageMetadata(
+        // Set page metadata
+        this.page = new PageMetadata(
                 pageMetadata.getSize(),
                 pageMetadata.getNumber(),
                 pageMetadata.getTotalElements(),
                 pageMetadata.getTotalPages()
         );
         
-        this.links = new Links();
+        // Set links
+        this.links = new HashMap<>();
         for (Link link : links) {
-            switch(link.getRel().value()) {
-                case "self":
-                    this.links.setSelf(link.getHref());
-                    break;
-                case "first":
-                    this.links.setFirst(link.getHref());
-                    break;
-                case "prev":
-                    this.links.setPrev(link.getHref());
-                    break;
-                case "next":
-                    this.links.setNext(link.getHref());
-                    break;
-                case "last":
-                    this.links.setLast(link.getHref());
-                    break;
-            }
+            this.links.put(link.getRel().value(), new LinkItem(link.getHref()));
+        }
+    }
+    
+    @Data
+    public static class Embedded implements Serializable {
+        private static final long serialVersionUID = 1L;
+        
+        @JsonProperty("pemantauanDasList")
+        private List<PemantauanDasDTO> pemantauanDasList;
+        
+        public Embedded() {
+            this.pemantauanDasList = new ArrayList<>();
+        }
+        
+        public Embedded(List<PemantauanDasDTO> pemantauanDasList) {
+            this.pemantauanDasList = pemantauanDasList;
+        }
+    }
+    
+    @Data
+    public static class LinkItem implements Serializable {
+        private static final long serialVersionUID = 1L;
+        
+        private String href;
+        
+        public LinkItem() {
+            this.href = "";
+        }
+        
+        public LinkItem(String href) {
+            this.href = href;
         }
     }
     
@@ -70,9 +97,9 @@ public class PemantauanDasPageDTO implements Serializable {
         private static final long serialVersionUID = 1L;
         
         private long size;
-        private long number;
         private long totalElements;
         private long totalPages;
+        private long number;
         
         // Default constructor for deserialization
         public PageMetadata() {
@@ -84,26 +111,6 @@ public class PemantauanDasPageDTO implements Serializable {
             this.number = number;
             this.totalElements = totalElements;
             this.totalPages = totalPages;
-        }
-    }
-    
-    @Data
-    public static class Links implements Serializable {
-        private static final long serialVersionUID = 1L;
-        
-        private String self;
-        private String first;
-        private String prev;
-        private String next;
-        private String last;
-        
-        // Default constructor for deserialization
-        public Links() {
-            this.self = "";
-            this.first = "";
-            this.prev = "";
-            this.next = "";
-            this.last = "";
         }
     }
 }
